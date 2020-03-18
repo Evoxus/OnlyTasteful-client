db7b7163536993f2525d2f38d774ae563887419a
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-17 22:03:56 -0600

    Create RecipeItem component

---
+++ src/RecipeItem/RecipeItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './RecipeItem.css';
+
+function RecipeItem() {
+  return (
+    <div>
+      <h4>RecipeTitle</h4>
+      <p>RecipeAuthor</p>
+      <p>[<em>Img placeholder</em>]</p>
+      <p>Short Description</p>
+    </div>
+  )
+}
+
+export default RecipeItem
