463126c96c159cd11fab8f713e8bd9980dd2f579
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-17 22:21:35 -0600

    Convert RecipeTitle to link

--- src/RecipeItem/RecipeItem.js
+++ src/RecipeItem/RecipeItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './RecipeItem.css';
 
 function RecipeItem() {
   return (
     <div>
-      <h4>RecipeTitle</h4>
+      <h4><Link to='/recipes/1'>RecipeTitle</Link></h4>
       <p>RecipeAuthor</p>
       <p>[<em>Img placeholder</em>]</p>
       <p>Short Description</p>
