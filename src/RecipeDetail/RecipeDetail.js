a3da6a4ebfffa7a5d784e88b96ce7e541986255d
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-21 13:59:55 -0600

    Add context to RecipeDetail and RecipeList

--- src/RecipeDetail/RecipeDetail.js
+++ src/RecipeDetail/RecipeDetail.js
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RecipeDetail.css';
-import { findUser } from '../recipe-helpers';
+import { findUser, findRecipe } from '../recipe-helpers';
+import OnlyTastefulContext from '../OnlyTastefulContext';
 
-function RecipeDetail(props) {
-  const ingredients = props.data.ingredients.map((ingredient, idx) => 
-    <li key={idx}>{ingredient.quantity} {ingredient.unit} {ingredient.name}</li>
-  )
-  const username = findUser(props.users, props.data.user_id).user_name
-  return(
-    <main className='RecipeDetail'>
-      <header>
-        <h2>{props.data.title}</h2>
-        <p>[<em>Img placeholder</em>]</p>
-        <p>added by: {username}</p>
-      </header>
-      <section className="description">
-        <p>
-          {props.data.description}
-        </p>
-      </section>
-      <section>
-        <h3>Ingredients List</h3>
-        <ul>
-          {ingredients}
-        </ul>
-      </section>
-      <section>
-        <h3>Cooking Instructions</h3>
-        <p>
-          {props.data.instructions}
-        </p>
-      </section>
-    </main>
-  )
+class RecipeDetail extends Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
+  }
+
+  static contextType = OnlyTastefulContext;
+
+  render() {
+    console.log('params', this.props.match.params)
+    const { recipeId } = this.props.match.params;
+    const { users } = this.context;
+    const recipe = findRecipe(this.context.recipes, recipeId)
+    const ingredients = recipe.ingredients.map((ingredient, idx) =>
+      <li key={idx}>{ingredient.quantity} {ingredient.unit} {ingredient.name}</li>
+    )
+    const username = findUser(users, recipe.user_id).user_name
+    return (
+      <main className='RecipeDetail'>
+        <header>
+          <h2>{recipe.title}</h2>
+          <p>[<em>Img placeholder</em>]</p>
+          <p>added by: {username}</p>
+        </header>
+        <section className="description">
+          <p>
+            {recipe.description}
+          </p>
+        </section>
+        <section>
+          <h3>Ingredients List</h3>
+          <ul>
+            {ingredients}
+          </ul>
+        </section>
+        <section>
+          <h3>Cooking Instructions</h3>
+          <p>
+            {recipe.instructions}
+          </p>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default RecipeDetail;
--- src/RecipeList/RecipeList.js
+++ src/RecipeList/RecipeList.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RecipeItem from '../RecipeItem/RecipeItem';
+import OnlyTastefulContext from '../OnlyTastefulContext';
 import './RecipeList.css';
 
-function RecipeList(props) {
-  const recipes = props.recipes.map((recipe, idx) => 
-    <RecipeItem key={idx} data={recipe} users={props.users} />
-  );
-  return (
-    <section className='recipeList'>
-      {recipes}
-    </section>
-  )
+class RecipeList extends Component {
+  
+  static contextType = OnlyTastefulContext;
+
+  render() {
+    const recipes = this.context.recipes.map((recipe, idx) =>
+      <RecipeItem key={idx} data={recipe} users={this.context.users} />
+    );
+    return (
+      <section className='recipeList'>
+        {recipes}
+      </section>
+    )
+  }
 }
 
 export default RecipeList
