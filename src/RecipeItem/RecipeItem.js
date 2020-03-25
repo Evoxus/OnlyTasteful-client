2453d95c6caea80164336bd10a1fe52ee5e8969a
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-24 19:51:12 -0600

    Minor styling to RecipeList and RecipeItem Added "Added by: " to recipe item for clarity

--- src/RecipeItem/RecipeItem.js
+++ src/RecipeItem/RecipeItem.js
@@ -6,9 +6,9 @@ import { findUser } from '../recipe-helpers';
 function RecipeItem(props) {
   const userName = findUser(props.users, props.data.user_id).user_name
   return (
-    <div>
+    <div className='recipeItem'>
       <h4><Link to={`/recipes/${props.data.id}`}>{props.data.title}</Link></h4>
-      <p>{userName}</p>
+      <p>Added by: {userName}</p>
       <p>[<em>Img placeholder</em>]</p>
       <p>{props.data.description.slice(0, 51) + '...'}</p>
     </div>
