8e4e81ca1b6d76798e263cbf89302f5042825cf0
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-24 19:35:19 -0600

    Style changes to Landing Fix console error in IngredientInput about defaultValue for props

--- src/IngredientInput/IngredientInput.js
+++ src/IngredientInput/IngredientInput.js
@@ -6,11 +6,11 @@ export default function IngredientInput(props) {
     <div key={props.idx} className='IngredientInput' onChange={(e) => props.handleChange(e.target, props.idx)}>
       <p>Ingredient {props.idx + 1}</p>
       <label htmlFor={`ingredient${props.idx}`}>Name</label>
-      <input type='text' name={`ingredient_name${props.idx}`} id={`ingredient_name${props.idx}`} value={props.data.name}/>
+      <input type='text' name={`ingredient_name${props.idx}`} id={`ingredient_name${props.idx}`} defaultValue={props.data.name}/>
       <label htmlFor={`ingredient${props.idx}`}>Quantity</label>
-      <input type='text' name={`ingredient_quantity${props.idx}`} id={`ingredient_quantity${props.idx}`} value={props.data.quantity}/>
+      <input type='text' name={`ingredient_quantity${props.idx}`} id={`ingredient_quantity${props.idx}`} defaultValue={props.data.quantity}/>
       <label htmlFor={`ingredient${props.idx}`}>Unit</label>
-      <input type='text' name={`ingredient_unit${props.idx}`} id={`ingredient_unit${props.idx}`} value={props.data.unit} />
+      <input type='text' name={`ingredient_unit${props.idx}`} id={`ingredient_unit${props.idx}`} defaultValue={props.data.unit} />
       { props.arrLength > 1 ? 
         <button type="button"
           onClick={(e) => props.onClick(e)}
