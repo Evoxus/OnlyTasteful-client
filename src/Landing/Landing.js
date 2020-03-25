df8b9ab3ca7a54fc8eaabc0831ee0dea329aad1e
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-24 19:29:34 -0600

    Refactor landing page descriptions into links Minor styling changes to accompany change to links

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Landing.css';
+import { Link } from 'react-router-dom';
 
 function Landing() {
   return (
@@ -9,18 +10,22 @@ function Landing() {
         <h2>A cookbook for the modern cook</h2>
       </section>
       <section className="findYourDish">
-        <h3>Find your next dish to cook</h3>
-        <p>
-          [<em>Placeholder for screenshot of view all recipes</em>] OnlyTasteful is the digital
+        <Link to='/recipes' className='hoverEffect'>
+          <h3>Find your next dish to cook</h3>
+          <p>
+            [<em>Placeholder for screenshot of view all recipes</em>] OnlyTasteful is the digital
           equivalent of grandma's cookbook.
         </p>
+        </Link>
       </section>
       <section className="createRecipes">
-        <h3>Create and modify your own recipes</h3>
-        <p>
-          [<em>Placeholder for gif of create/modify interface</em>]
+        <Link to='/signin' className='hoverEffect'>
+          <h3>Create and modify your own recipes</h3>
+          <p>
+            [<em>Placeholder for gif of create/modify interface</em>]
           Add or modify your recipes to build a library of delicious meals.
         </p>
+        </Link>
       </section>
     </main>
   )
