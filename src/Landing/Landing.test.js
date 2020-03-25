74c4a97114deb54585b8546a23d329ee8a1d9b64
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-24 20:17:04 -0600

    Fixed landing test, now passing

--- src/Landing/Landing.test.js
+++ src/Landing/Landing.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Landing from './Landing';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Landing Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Landing />, div);
+    ReactDOM.render(<BrowserRouter> <Landing /> </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   })
 })
