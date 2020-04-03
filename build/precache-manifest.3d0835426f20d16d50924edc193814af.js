482b08cf4a250c4fd19235d54637d54490b4c1e2
Michael Ploughman <michaelhploughman@gmail.com>
2020-04-03 16:23:52 -0600

    Add build folder to repo for Zeit

---
+++ build/precache-manifest.3d0835426f20d16d50924edc193814af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3b3b619f0a7a92ef94528448609d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d27d43afba0d3041b2",
+    "url": "/static/css/main.727cddee.chunk.css"
+  },
+  {
+    "revision": "3e33e7b5aa07c149ad8d",
+    "url": "/static/js/2.6a03c63f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6a03c63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d27d43afba0d3041b2",
+    "url": "/static/js/main.7df6502d.chunk.js"
+  },
+  {
+    "revision": "4b1d415aa9eb33036703",
+    "url": "/static/js/runtime-main.0cbc8601.js"
+  }
+]);
