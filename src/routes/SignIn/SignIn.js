2cb68f573af6a274eeebd5849804a24aeb963a5c
Michael Ploughman <michaelhploughman@gmail.com>
2020-04-06 10:48:24 -0600

    Add SignIn form validation

--- src/routes/SignIn/SignIn.js
+++ src/routes/SignIn/SignIn.js
@@ -8,8 +8,17 @@ import './SignIn.css';
 
 export default class SignIn extends Component {
   state = {
+    user_name: {
+      value: '',
+      touched: false,
+    },
+    password: {
+      value: '',
+      touched: false,
+    },
     error: null,
   }
+
   static defaultProps = {
     history: {
       push: () => { }
@@ -39,6 +48,46 @@ export default class SignIn extends Component {
       }))
   }
 
+  handleUsernameChange = (value) => {
+    this.setState({
+      user_name: {
+        value: value,
+        touched: true,
+      }
+    })
+  }
+
+  handlePasswordChange = (value) => {
+    this.setState({
+      password: {
+        value: value,
+        touched: true,
+      },
+    })
+  }
+
+  usernameValidation = () => {
+    const { user_name } = this.state
+    if (user_name.value.length <= 3) {
+      return 'Username must be at least 3 characters long'
+    }
+    return false
+  }
+
+  passwordValidation = () => {
+    const { password } = this.state
+    if (password.value.length <= 7) {
+      return 'Password needs to be 8 or more characters'
+    }
+    if (password.length > 72) {
+      return 'Password must be less than 72 characters'
+    }
+    if (password.value.startsWith(' ') || password.value.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces'
+    }
+    return false
+  }
+
   render() {
     return (
       <main className='signIn'>
@@ -50,15 +99,19 @@ export default class SignIn extends Component {
           <p className='demoUserCredentials'>* both Username and Password are case-sensitive</p>
         </header>
         <section>
-          { this.state.error && <div className='errorMsg'>{this.state.error}</div>}
-        <form className='signInForm' onSubmit={this.onSignIn}>
-          <label htmlFor='signin_user_name'>Username</label>
-          <input type='text' name='user_name' id='signin_user_name' />
-          <label htmlFor='signin_password'>Password</label>
-          <input type='password' name='password' id='signin_password' />
-          <button type='submit'>Sign In</button>
-        </form>
-      </section>
+          {this.state.error && <div className='errorMsg'>{this.state.error}</div>}
+          <form className='signInForm' onSubmit={this.onSignIn}>
+            <label htmlFor='signin_user_name'>Username</label>
+            <input type='text' name='user_name' id='signin_user_name'
+              onChange={e => this.handleUsernameChange(e.target.value)} />
+            {this.state.user_name.touched && <div className='formValidationMsg'>{this.usernameValidation()}</div>}
+            <label htmlFor='signin_password'>Password</label>
+            <input type='password' name='password' id='signin_password'
+              onChange={e => this.handlePasswordChange(e.target.value)} />
+            {this.state.password.touched && <div className='formValidationMsg'>{this.passwordValidation()}</div>}
+            <button disabled={this.usernameValidation() || this.passwordValidation()} type='submit'>Sign In</button>
+          </form>
+        </section>
       </main>
     )
   }
