b7120b0f7c53036caa4fc1a8e4734a68ea0f4d43
Michael Ploughman <michaelhploughman@gmail.com>
2020-04-06 11:00:55 -0600

    Add SignUp validation

--- src/routes/SignUp/SignUp.js
+++ src/routes/SignUp/SignUp.js
@@ -8,6 +8,18 @@ import './SignUp.css';
 
 export default class SignUp extends Component {
   state = {
+    user_name: {
+      value: '',
+      touched: false
+    },
+    password: {
+      value: '',
+      touched: false,
+    },
+    full_name: {
+      value: '',
+      touched: false,
+    },
     error: null,
   }
   static defaultProps = {
@@ -47,6 +59,68 @@ export default class SignUp extends Component {
       }))
   }
 
+  handleUsernameChange = (value) => {
+    this.setState({
+      user_name: {
+        value: value,
+        touched: true,
+      }
+    })
+  }
+
+  handlePasswordChange = (value) => {
+    this.setState({
+      password: {
+        value: value,
+        touched: true,
+      },
+    })
+  }
+
+  handleFullNameChange = (value) => {
+    this.setState({
+      full_name: {
+        value: value,
+        touched: true,
+      }
+    })
+  }
+
+  usernameValidation = () => {
+    const { user_name } = this.state
+    if (user_name.value.length <= 3) {
+      return 'Username must be at least 3 characters long'
+    }
+    return false
+  }
+
+  fullNameValidation = () => {
+    const { full_name } = this.state
+    if (full_name.value.length <= 3) {
+      return 'Full name must be at least 3 characters long'
+    }
+    return false
+  }
+
+  passwordValidation = () => {
+    const { password } = this.state
+    // eslint-disable-next-line
+    const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+    if (password.value.length <= 7) {
+      return 'Password needs to be 8 or more characters'
+    }
+    if (password.value.length > 72) {
+      return 'Password must be less than 72 characters'
+    }
+    if (password.value.startsWith(' ') || password.value.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces'
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password.value)) {
+      return 'Password must contain 1 upper case, lower case, number and special character'
+    }
+    return false
+  }
+
   render() {
     return (
       <main role='main' className='signUp'>
@@ -57,12 +131,19 @@ export default class SignUp extends Component {
           {this.state.error && <div className='errorMsg'>{this.state.error}</div>}
           <form className='signUpForm' onSubmit={this.onSignUp}>
             <label htmlFor='register_user_name'>Username</label>
-            <input type='text' name='user_name' id='register_user_name' />
+            <input type='text' name='user_name' id='register_user_name'
+              onChange={e => this.handleUsernameChange(e.target.value)} />
+            {this.state.user_name.touched && <div className='formValidationMsg'>{this.usernameValidation()}</div>}
             <label htmlFor='register_password'>Password</label>
-            <input type='password' name='password' id='register_password' />
+            <input type='password' name='password' id='register_password' 
+              onChange={e => this.handlePasswordChange(e.target.value)} />
+            {this.state.password.touched && <div className='formValidationMsg'>{this.passwordValidation()}</div>}
             <label htmlFor='register_full_name'>Full Name</label>
-            <input type='text' name='full_name' id='register_full_name' />
-            <button type='submit'>Sign Up</button>
+            <input type='text' name='full_name' id='register_full_name'
+              onChange={e => this.handleFullNameChange(e.target.value)} />
+            {this.state.full_name.touched && <div className='formValidationMsg'>{this.fullNameValidation()}</div>}
+            <button disabled={this.usernameValidation() || this.passwordValidation() || this.fullNameValidation()}
+              type='submit'>Sign Up</button>
           </form>
         </section>
       </main>
